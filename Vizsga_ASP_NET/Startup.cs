828353fb6cf066cda83bc2119f1a8f7926ae92fe
MartonVarga <69591271+MartonVarga@users.noreply.github.com>
2022-04-06 16:24:58 +0200

    Full boiler plate, without Postman collection

--- Vizsga_ASP_NET/Startup.cs
+++ Vizsga_ASP_NET/Startup.cs
@@ -25,6 +25,7 @@ namespace Vizsga_ASP_NET
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
